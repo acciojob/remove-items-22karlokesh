abccea83886774a6152218858a759642f29f6788
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-06-03 02:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function removeOption() {
+	const select = document.getElementById("colorSelect");
+	let index = select.selectedIndex;
+
+	select.remove(index);
+	
+}
